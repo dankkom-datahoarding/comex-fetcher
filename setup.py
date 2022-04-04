4a89854806bd876028fdb084604337f1328c3704
Daniel Komesu <danielkomesu@gmail.com>
2022-04-04 15:38:45 -0300

    Drop Poetry as dependency manager

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="secex-comex",
+    version="0.0.1",
+    author="Daniel Komesu",
+    author_email="danielkomesu@gmail.com",
+    description="Downloads the data from the SECEX website.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    requires=["requests"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
